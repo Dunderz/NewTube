0fe7a5f9eb6e3207705d9f41580b225128466ac7
Isaac Nam <ikn208@nyu.edu>
2020-04-06 17:57:28 -0400

    migrated videos

---
+++ db/migrate/20200406215533_create_videos.rb
@@ -0,0 +1,13 @@
+class CreateVideos < ActiveRecord::Migration[5.2]
+  def change
+    create_table :videos do |t|
+      t.string :title, null: false
+      t.integer :author_id, null: false
+      
+      t.timestamps
+    end
+
+    add_index :videos, :title, unique: true
+    add_index :videos, :author_id, unique: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_06_214857) do
+ActiveRecord::Schema.define(version: 2020_04_06_215533) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,15 @@ ActiveRecord::Schema.define(version: 2020_04_06_214857) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["session_token"], name: "index_users_on_session_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
+  end
+
+  create_table "videos", force: :cascade do |t|
+    t.string "title", null: false
+    t.integer "author_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["author_id"], name: "index_videos_on_author_id", unique: true
+    t.index ["title"], name: "index_videos_on_title", unique: true
   end
 
 end
