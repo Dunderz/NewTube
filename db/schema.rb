77e44c0ff395a2ff6b3d55d4e59dc64084d69f03
Isaac Nam <ikn208@nyu.edu>
2020-04-06 17:54:34 -0400

    migrated comments and likes

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_06_213954) do
+ActiveRecord::Schema.define(version: 2020_04_06_214857) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.integer "video_id", null: false
+    t.integer "user_id", null: false
+    t.string "body", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "likes", force: :cascade do |t|
+    t.boolean "is_like", null: false
+    t.integer "video_id", null: false
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_likes_on_user_id", unique: true
+    t.index ["video_id"], name: "index_likes_on_video_id", unique: true
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username", null: false
