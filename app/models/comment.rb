eacb51c75c2bbd5789a705ae631af33094145692
Isaac Nam <ikn208@nyu.edu>
2020-04-07 00:17:31 -0400

    removed session from model, still need to complete user and video models

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -12,6 +12,11 @@
 class Comment < ApplicationRecord
     validates :body, presence: true
 
-    belongs_to :user
-    belongs_to :video
+    belongs_to :user,
+        class_name: :User,
+        foreign_key: :user_id
+
+    belongs_to :video,
+        class_name: :Video,
+        foreign_key: :video_id
 end
