95972a3194bb1949a30b85de9906aa99eb3ea4f4
Isaac Nam <ikn208@nyu.edu>
2020-04-07 09:47:14 -0400

    finished user and video models

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,5 +11,52 @@
 #  updated_at      :datetime         not null
 #
 class User < ApplicationRecord
+    validates :username, presence: true, uniqueness: true
+    validates :password_digest, presence: true
+    validates :session_token, presence: true, uniqueness: true
+    validates :email, presence: true, uniqueness: true
+    validates :password, length: { minimum: 6 }
 
+    has_many :comments,
+        foreign_key: :video_id,
+        class_name: :Comment
+
+    has_many :likes,
+        foreign_key: :video_id,
+        class_name: :Like
+
+    has_many :videos,
+        foreign_key: :author_id,
+        class_name: :Video
+        
+    attr_reader :password
+
+    after_initialize :ensure_session_token
+
+    def self.find_by_credentials(username, password)
+        user = User.find_by(username: username)
+        return nil unless user && user.is_password?(password)
+    end
+
+    def self.generate_session_token
+        SecureRandom::urlsafe_base64
+    end
+
+    def reset_session_token!
+        self.update!(session_token: User.generate_session_token)
+    end
+
+    def is_password?(password)
+        bcrypt_password = BCrypt::Password.new(self.password_digest)
+        bcrypt_password.is_password?(password)
+    end
+
+    def password=(password)
+        @password = password
+        self.password_digest = BCrypt::Password.create(password)
+    end
+
+    def ensure_session_token
+        self.session_token || User.generate_session_token
+    end
 end
--- app/models/video.rb
+++ app/models/video.rb
@@ -9,5 +9,18 @@
 #  updated_at :datetime         not null
 #
 class Video < ApplicationRecord
+    validates :title, presence: true, uniqueness: true
+    validates :author_id, presence: true
 
+    belongs_to :author,
+        foreign_key: :author_id,
+        class_name: :User
+
+    has_many :comments,
+        foreign_key: :video_id,
+        class_name: :Comment
+
+    has_many :likes,
+        foreign_key: :video_id,
+        class_name: :Like
 end
