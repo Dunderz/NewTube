124d24927e00cfadd1195fde47bf80222457fe73
Isaac Nam <ikn208@nyu.edu>
2020-04-07 11:57:51 -0400

    large portion of frontend auth completed

--- frontend/new_tube.jsx
+++ frontend/new_tube.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Root from './components/root';
+import configureStore from './store/store';
 
 document.addEventListener('DOMContentLoaded', () => {
+    const store = configureStore();
     console.log("yo");
     const root = document.getElementById('root');
-    ReactDOM.render(<h1>hello</h1>, root);
+    ReactDOM.render(<Root store={store}/>, root);
 });
---
+++ frontend/store/store.js
@@ -0,0 +1,10 @@
+import { applyMiddleware, createStore } from 'redux';
+import thunk from 'redux-thunk';
+import RootReducer from '../reducers/root';
+import logger from 'redux-logger';
+
+const configureStore = (preloadedState = {}) => (
+    createStore(RootReducer, preloadedState, applyMiddleware(thunk, logger))
+);
+
+export default configureStore;
