910f501b54cdc2952b4eea4965e0ee40fed601dc
Isaac Nam <ikn208@nyu.edu>
2020-04-08 13:44:21 -0400

    starting to style navbar

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -3,13 +3,16 @@ import { Route, Switch } from 'react-router-dom';
 import SignUpContainer from './session/signup_container';
 import NavBarContainer from './nav_bar/nav_bar_container';
 import LoginContainer from './session/login_container';
+import { AuthRoute, ProtectedRoute } from '../util/route_util';
 
-export default () => (
+const App = () => (
     <div>
         <Switch>
-            <Route exact path="/login" component={LoginContainer}/>
-            <Route exact path="/signup" component={SignUpContainer} />
-            <Route path="/" component={NavBarContainer}/>
+            <AuthRoute exact path="/login" component={LoginContainer}/>
+            <AuthRoute exact path="/signup" component={SignUpContainer} />
+            <Route exact path="/" component={NavBarContainer}/>
         </Switch>
     </div>
-)
+);
+
+export default App;
