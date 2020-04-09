d6dd829ab80cf649ebe2076e23fe4d19c8c53c2c
Isaac Nam <ikn208@nyu.edu>
2020-04-08 23:22:17 -0400

    stupid turbolinks, almost done with css for auth

--- frontend/components/session/signup.jsx
+++ frontend/components/session/signup.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class Signup extends React.Component {
     constructor(props) {
@@ -30,29 +31,36 @@ class Signup extends React.Component {
                 <div className="form-container">
                     <form className="form-sign-up">
                         <img src={ window.newtube } id='signup-logo'/>
-                        <h2>Sign Up</h2>
-                        <label>Username: 
+                        <h2 className="sign-in-title">Create your NewTube</h2>
+                        <p className="sign-in-subtitle">to continue to NewTube</p>
+                        <label className="input-username">
                             <input 
                                 type="text" 
                                 value={this.state.username}
                                 onChange={this.handleInput('username')}
+                                placeholder="Username"
                                 />
                         </label>
-                        <label>Email: 
+                        <label className="input-email">
                             <input 
                                 type="text" 
                                 value={this.state.email}
                                 onChange={this.handleInput('email')}
+                                placeholder="Email"
                                 />
                         </label>
-                        <label>Password: 
+                        <label className="input-password">
                             <input 
                                 type="password" 
                                 value={this.state.password}
                                 onChange={this.handleInput('password')}
+                                placeholder="Password"
                                 />
                         </label>
-                        <button onClick={this.handleSubmit}>Sign Up</button>
+                        <div className="form-buttons">
+                            <Link className="sign-up-link" to="/login">Sign in instead</Link>
+                            <button className="log-in-btn" onClick={this.handleSubmit}>Sign Up</button>
+                        </div>
                     </form>
                 </div>
             </div>
