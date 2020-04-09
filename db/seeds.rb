110a57e8801b4f4d3c039f074f66f63040569094
Isaac Nam <ikn208@nyu.edu>
2020-04-09 10:56:05 -0400

    seeded demo user

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,4 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+demo = User.create!(username:"demo", email:"demo@gmail.com", password:"password")
