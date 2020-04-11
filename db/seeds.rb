6d705f8cbe7f55b86057a74cfa19814751082453
Isaac Nam <ikn208@nyu.edu>
2020-04-11 13:06:33 -0400

    testing seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# demo = User.create!(username:"demo", email:"demo@gmail.com", password:"password")
+demo = User.create!(username:"demo", email:"demo@gmail.com", password:"password")
 v = Video.create!(title: "look at omar", author_id: 1)
 v.video.attach(io: File.open("/mnt/c/Users/Isaac\ Nam/Desktop/newtubevids/omardance.mp4"), filename: "omardance.mp4")
