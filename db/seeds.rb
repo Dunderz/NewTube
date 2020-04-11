4b0f64f82f59e917fd2d0525e3e439ce4ca7fd1d
Isaac Nam <ikn208@nyu.edu>
2020-04-11 16:10:51 -0400

    gitignore, seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+require "open-uri"
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -12,4 +13,4 @@ User.destroy_all
 demo = User.create!(username:"demo", email:"demo@gmail.com", password:"password")
 v = Video.create!(title: "look at omar", author_id: 1)
 # v.video.attach(io: File.open("/mnt/c/Users/Isaac\ Nam/Desktop/newtubevids/omardance.mp4"), filename: "omardance.mp4")
-v.video.attach(io: open("https://newtube-dev.s3.amazonaws.com/aWcqmmApgBxgQCLAr1w2ad4i"), filename: "omardance.mp4")
+v.video.attach(io: open("https://newtube-dev.s3.amazonaws.com/omardance.mp4"), filename: "omardance.mp4")
