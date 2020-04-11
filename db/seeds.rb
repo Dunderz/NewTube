db3d57d3680ff6acf4cc6081d8a82b98f5b97727
Isaac Nam <ikn208@nyu.edu>
2020-04-11 13:45:35 -0400

    seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,4 +11,5 @@ User.destroy_all
 
 demo = User.create!(username:"demo", email:"demo@gmail.com", password:"password")
 v = Video.create!(title: "look at omar", author_id: 1)
-v.video.attach(io: open("https://newtube-dev.s3.amazonaws.com/6WCKWMGAznALUFxi8cWvw2in"), filename: "omardance.mp4")
+# v.video.attach(io: File.open("/mnt/c/Users/Isaac\ Nam/Desktop/newtubevids/omardance.mp4"), filename: "omardance.mp4")
+v.video.attach(io: open("https://newtube-dev.s3.amazonaws.com/aWcqmmApgBxgQCLAr1w2ad4i"), filename: "omardance.mp4")
