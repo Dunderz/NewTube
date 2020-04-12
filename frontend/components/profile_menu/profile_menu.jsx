2f9bb14bb8e8571aa798b4181924d57c6f3ab191
Isaac Nam <ikn208@nyu.edu>
2020-04-12 18:47:23 -0400

    constructed profile dropdown

--- frontend/components/profile_menu/profile_menu.jsx
+++ frontend/components/profile_menu/profile_menu.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faUserCircle } from '@fortawesome/free-solid-svg-icons';
+import { faUserCircle, 
+         faSignOutAlt,
+         faUser} from '@fortawesome/free-solid-svg-icons';
+import { faGithubSquare } from '@fortawesome/free-brands-svg-icons';
 
 class ProfileMenu extends React.Component {
     constructor(props) {
@@ -9,8 +12,12 @@ class ProfileMenu extends React.Component {
         this.state = {
             active: false
         }
-
+        this.handleClick = this.handleClick.bind(this);
         this.toggleActive = this.toggleActive.bind(this);
+    }
+
+    handleClick(e) {
+        window.location.assign("https://github.com/DunderProto/NewTube");
     }
 
     toggleActive(e) {
@@ -30,7 +37,40 @@ class ProfileMenu extends React.Component {
                     <p className="profile-letter">{this.props.currentUser.username[0].toUpperCase()}</p>
                 </div>
                 <div className={isActive}>
-                    <button onClick={this.props.logout}>Logout</button>
+                    <div className="dropdown-user-info">
+                        <div className="user-icon">
+                            <FontAwesomeIcon icon={faUserCircle} />
+                        </div>
+                        <div className="dropdown-credentials">
+                            <div className="dropdown-username">
+                                {this.props.currentUser.username}
+                            </div>
+                            <div className="dropdown-email">
+                                {this.props.currentUser.email}
+                            </div>
+                            <div className="dropdown-google-account">
+                                <p>Manage your Google Account</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="dropdown-channel">
+                        <div className="dropdown-icon">
+                            <FontAwesomeIcon icon={faUser} />
+                        </div>
+                        <div>Your Channel</div>
+                    </div>
+                    <div onClick={this.handleClick} className="dropdown-github">
+                        <div className="dropdown-icon">
+                            <FontAwesomeIcon icon={faGithubSquare} />
+                        </div>
+                        <div>Github</div>
+                    </div>
+                    <div onClick={this.props.logout} className="dropdown-logout">
+                        <div className="dropdown-icon">
+                            <FontAwesomeIcon icon={faSignOutAlt} />
+                        </div>
+                        <div>Sign Out</div>
+                    </div>
                 </div>
             </div>
         )
