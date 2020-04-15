06af67f8a2cec066bca9a75ff4b3c64bccbab386
Isaac Nam <ikn208@nyu.edu>
2020-04-15 12:11:26 -0400

    styling videoshow, just did videoshow date

--- frontend/components/video/video_show.jsx
+++ frontend/components/video/video_show.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class VideoShow extends React.Component {
     constructor(props) {
@@ -7,6 +8,7 @@ class VideoShow extends React.Component {
 
     componentDidMount() {
         this.props.requestVideo(this.props.match.params.id);
+        this.props.requestVideos();
     }
 
     render() {
@@ -19,7 +21,8 @@ class VideoShow extends React.Component {
         }
 
         const { video } = this.props;
-        
+        const upNextVideo = this.props.videos.shift();
+
         return (
             
             <div className="videoshow-container">
@@ -31,11 +34,72 @@ class VideoShow extends React.Component {
                         <div className="videoshow-title">
                             {video.title}
                         </div>
+                        
+                        <div className="videoshow-views-likes-options">
+                            <div className="videoshow-views">
+                                <h2>Test views</h2>
+                                <span className="videoshow-views-dot"></span>
+                            </div>
+
+                            <div className="videoshow-date">
+                                <h2>Jan 23, 2020</h2>
+                            </div>
+
+                            <div className="videoshow-thumbs-up">
+                                
+                            </div>
+
+                            <div className="videoshow-thumbs-down">
+                                
+                            </div>
+
+                            <div className="videoshow-share">
+
+                            </div>
+
+                            <div className="videoshow-save">
+
+                            </div>
+
+                            <div className="videoshow-more-options">
+
+                            </div>
+                        </div>
                     </div>
                 </div>
 
                 <div className="videoshow-right-box">
-
+                    <div className="videoshow-up-next-container">
+                        <p>Up Next</p>
+                        <div className="videoshow-up-next-video">
+                            <Link key={upNextVideo.id} className="videoshow-link" to={`/videos/${upNextVideo.id}`}>                  
+                            <div className="videoshow-index">
+                                <video src={upNextVideo.videoUrl} controls={false}/>
+                            </div>
+                            <div className="videoshow-rec-info">
+                                <h2>{upNextVideo.title}</h2>
+                            </div>
+                            </Link> 
+                        </div>
+                    </div>
+                    <div className="videoshow-recommended">
+                        {this.props.videos.map(video => {
+                            if (video.id !== this.props.video.id) {
+                                return (
+                                    <div className="videoshow-up-next-video"> 
+                                        <Link key={video.id} className="videoshow-link" to={`/videos/${video.id}`}>                  
+                                        <div className="videoshow-index">
+                                            <video src={video.videoUrl} controls={false}/>
+                                        </div>
+                                        <div className="videoshow-rec-info">
+                                            <h2>{video.title}</h2>
+                                        </div>
+                                        </Link> 
+                                    </div>
+                                );
+                            }
+                        })}
+                    </div>
                 </div>
             </div>
         );
--- frontend/components/video/video_show_container.js
+++ frontend/components/video/video_show_container.js
@@ -1,16 +1,18 @@
 import { connect } from 'react-redux';
-import { requestVideo } from '../../actions/video_actions'
+import { requestVideo, requestVideos } from '../../actions/video_actions'
 import VideoShow from './video_show';
 
 const mapStateToProps = (state, ownProps) => {
     return {
-        video: state.entities.videos[ownProps.match.params.id]
+        video: state.entities.videos[ownProps.match.params.id],
+        videos: Object.values(state.entities.videos)
     }
 }
 
 const mapDispatchToProps = dispatch => {
     return {
-        requestVideo: (id) => dispatch(requestVideo(id))
+        requestVideo: (id) => dispatch(requestVideo(id)),
+        requestVideos: () => dispatch(requestVideos())
     }
 }
 
