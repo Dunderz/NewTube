c43df69d61816098e2064b3a04236956f9cc8f7d
Isaac Nam <ikn208@nyu.edu>
2020-04-15 17:04:35 -0400

    added styling to video index page

--- frontend/components/video/video.jsx
+++ frontend/components/video/video.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUserCircle } from '@fortawesome/free-solid-svg-icons';
 
 class Video extends React.Component {
     constructor(props) {
@@ -36,7 +38,27 @@ class Video extends React.Component {
                             <video src={video.videoUrl} controls={false}/>
                           </div>
                           <div className="video-info">
-                            <h2>{video.title}</h2>
+                            <div className="video-info-top-row">
+                              <FontAwesomeIcon className="video-index-user-icon" icon={ faUserCircle}/>
+                              <div className="video-index-details">
+                                <div className="video-index-title">
+                                  {video.title}
+                                </div>
+                                <div className="video-index-uploader">
+                                  <p>Bob Jenkins</p>
+                                </div>
+                                <div className="video-index-views-date-container">
+                                  <div className="video-index-views">
+                                    2.2M views
+                                  </div>
+                                  <span className="videoshow-views-dot"></span>
+                                  <div className="video-index-date">
+                                    1 month ago
+                                  </div>
+                                </div>
+                              </div>
+                            </div>
+
                           </div>
                         </Link> 
                       </div>
