a955cd6c3622dc2a1ca97213845de225351fa9cb
Isaac Nam <ikn208@nyu.edu>
2020-04-15 15:27:19 -0400

    style most of video show page

--- frontend/components/video/video_show.jsx
+++ frontend/components/video/video_show.jsx
@@ -1,5 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { 
+    faThumbsUp, 
+    faThumbsDown,
+    faShare,
+    faListUl,
+    faEllipsisH,
+    faUserCircle,
+    faUser
+    } from '@fortawesome/free-solid-svg-icons';
 
 class VideoShow extends React.Component {
     constructor(props) {
@@ -28,7 +38,7 @@ class VideoShow extends React.Component {
             <div className="videoshow-container">
                 <div className="videoshow-left-box">
                     <div className="videoshow-player">
-                        <video id="video" src={video.videoUrl} controls/>
+                        <video id="video" src={video.videoUrl} controls autoPlay/>
                     </div>
                     <div className="videoshow-info">
                         <div className="videoshow-title">
@@ -39,30 +49,53 @@ class VideoShow extends React.Component {
                             <div className="videoshow-views">
                                 <h2>Test views</h2>
                                 <span className="videoshow-views-dot"></span>
+                                <div className="videoshow-date">
+                                    <h2>Jan 23, 2020</h2>
+                                </div>
                             </div>
+                            
+                            <div className="videoshow-likes-options">
+                                <div className="videoshow-thumbs-up hover">
+                                    <FontAwesomeIcon className="video-option-icons" icon={faThumbsUp}/>
+                                    <h2>0</h2>
+                                </div>
 
-                            <div className="videoshow-date">
-                                <h2>Jan 23, 2020</h2>
+                                <div className="videoshow-thumbs-down hover">
+                                    <FontAwesomeIcon className="video-option-icons" icon={faThumbsDown}/>
+                                    <h2>0</h2>
+                                </div>
+
+                                <div className="videoshow-share hover">
+                                    <FontAwesomeIcon className="video-option-icons" icon={faShare}/>
+                                    <h2>SHARE</h2>
+                                </div>
+
+                                <div className="videoshow-save hover">
+                                    <FontAwesomeIcon className="video-option-icons" icon={faListUl}/>
+                                    <h2>SAVE</h2>
+                                </div>
+
+                                <div className="videoshow-more-options hover">
+                                    <FontAwesomeIcon className="video-option-icons" icon={faEllipsisH}/>
+                                </div>
                             </div>
-
-                            <div className="videoshow-thumbs-up">
-                                
+                        </div>
+                    </div>
+                    <div className="videoshow-description">
+                        <div className="description-top-row">
+                            <div className="description-user-container hover">
+                                <FontAwesomeIcon className="description-user-icon" icon={faUserCircle} />
+                                <div className="description-user">
+                                    <div className="description-username">
+                                        <h2>TestUser</h2>
+                                    </div>
+                                    <div className="description-sub-count">
+                                        <h2>99.9K subscribers</h2>
+                                    </div>
+                                </div>
                             </div>
-
-                            <div className="videoshow-thumbs-down">
-                                
-                            </div>
-
-                            <div className="videoshow-share">
-
-                            </div>
-
-                            <div className="videoshow-save">
-
-                            </div>
-
-                            <div className="videoshow-more-options">
-
+                            <div className="subscribe hover">
+                                <h2>SUBSCRIBE</h2>
                             </div>
                         </div>
                     </div>
@@ -73,12 +106,24 @@ class VideoShow extends React.Component {
                         <p>Up Next</p>
                         <div className="videoshow-up-next-video">
                             <Link key={upNextVideo.id} className="videoshow-link" to={`/videos/${upNextVideo.id}`}>                  
-                            <div className="videoshow-index">
-                                <video src={upNextVideo.videoUrl} controls={false}/>
-                            </div>
-                            <div className="videoshow-rec-info">
-                                <h2>{upNextVideo.title}</h2>
-                            </div>
+                                <div className="videoshow-index">
+                                    <video src={upNextVideo.videoUrl} controls={false}/>
+                                </div>
+                                <div className="videoshow-rec-info">
+                                    <h2>{upNextVideo.title}</h2>
+                                    <div className="uploader-name">
+                                        <h2>Test Uploader</h2>
+                                    </div>
+                                    <div className="videoshow-rec-vid-stats">
+                                        <div className="videoshow-rec-views">
+                                            <h2>800K views</h2>
+                                            <span className="videoshow-views-dot"></span>
+                                        </div>
+                                        <div className="videoshow-rec-timeago">
+                                            <h2>2 months ago</h2>
+                                        </div>
+                                    </div>
+                                </div>
                             </Link> 
                         </div>
                     </div>
@@ -88,12 +133,24 @@ class VideoShow extends React.Component {
                                 return (
                                     <div className="videoshow-up-next-video"> 
                                         <Link key={video.id} className="videoshow-link" to={`/videos/${video.id}`}>                  
-                                        <div className="videoshow-index">
-                                            <video src={video.videoUrl} controls={false}/>
-                                        </div>
-                                        <div className="videoshow-rec-info">
-                                            <h2>{video.title}</h2>
-                                        </div>
+                                            <div className="videoshow-index">
+                                                <video src={video.videoUrl} controls={false}/>
+                                            </div>
+                                            <div className="videoshow-rec-info">
+                                                <h2>{video.title}</h2>
+                                                <div className="uploader-name">
+                                                    <h2>Test Uploader</h2>
+                                                </div>
+                                                <div className="videoshow-rec-vid-stats">
+                                                    <div className="videoshow-rec-views">
+                                                        <h2>800K views</h2>
+                                                        <span className="videoshow-views-dot"></span>
+                                                    </div>
+                                                    <div className="videoshow-rec-timeago">
+                                                        <h2>2 months ago</h2>
+                                                    </div>
+                                                </div>
+                                            </div>
                                         </Link> 
                                     </div>
                                 );
