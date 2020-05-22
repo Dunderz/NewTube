591e6b49edc0a4e1d52957dde4b14f522a1e6bf1
Isaac Nam <ikn208@nyu.edu>
2020-05-21 21:03:54 -0400

    working on comments controller

--- app/controllers/api/comments_controller.rb
+++ app/controllers/api/comments_controller.rb
@@ -1,3 +1,22 @@
 class Api::CommentsController < ApplicationController
-    
+    before_action :require_logged_in, only: [:create]
+
+    def index
+        @comments = Comment.where(video_id: params[:video_id])
+        render :index
+    end
+
+    def create
+        
+    end
+
+    def destroy
+
+    end
+
+    private
+
+    def comment_params
+        params.require(:comment).permit(:body, :video_id)
+    end
 end
