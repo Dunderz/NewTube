d7175874a0c8fb9b617c63c4f475d81f526c5c72
Isaac Nam <ikn208@nyu.edu>
2020-05-21 20:58:44 -0400

    added comments reducer

---
+++ frontend/reducers/entities/comments_reducer.js
@@ -0,0 +1,18 @@
+import { 
+    RECEIVE_COMMENTS, 
+    RECEIVE_COMMENT 
+} from '../../actions/comment_actions';
+
+const commentsReducer = ( state = {}, action ) => {
+    Object.freeze(state);
+    switch(action.type) {
+        case RECEIVE_COMMENTS:
+            return Object.assign({}, action.comments );
+        case RECEIVE_COMMENT:
+            return Object.assign({}, state, { [action.comment.id]: action.comment} );
+        default:
+            return state;
+    }
+}
+
+export default commentsReducer;
--- frontend/reducers/entities/entities_reducer.js
+++ frontend/reducers/entities/entities_reducer.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import videosReducer from './videos_reducer';
 import usersReducer from './users_reducer';
+import commentsReducer from './comments_reducer';
 
 const entitiesReducer = combineReducers({
     users: usersReducer,
-    videos: videosReducer
+    videos: videosReducer,
+    comments: commentsReducer
 });
 
 export default entitiesReducer;
