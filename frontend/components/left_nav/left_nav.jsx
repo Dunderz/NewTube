b849025771075b28154ceb1325163f36176d954e
Isaac Nam <ikn208@nyu.edu>
2020-05-29 14:07:14 -0400

    added left nav to home

--- frontend/components/left_nav/left_nav.jsx
+++ frontend/components/left_nav/left_nav.jsx
@@ -1,4 +1,15 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { Link } from 'react-router-dom';
+import { 
+    faHome,
+    faFolderOpen
+} from '@fortawesome/free-solid-svg-icons';
+import { 
+    faGithub,
+    faAngellist,
+    faLinkedin
+} from '@fortawesome/free-brands-svg-icons';
 
 class LeftNav extends React.Component {
     constructor(props) {
@@ -8,7 +19,28 @@ class LeftNav extends React.Component {
     render() {
         return (
             <>
-                <div className="left-nav-container"></div>
+                <div className="left-nav-container">
+                    <Link className="left-nav-link" to="/">
+                        <div className="left-nav-icon"><FontAwesomeIcon icon={faHome} /></div>
+                        <div className="left-nav-icon-title">Home</div>
+                    </Link>
+                    <a className="left-nav-link" href="https://github.com/DunderProto/NewTube" target="_blank">
+                        <div className="left-nav-icon"><FontAwesomeIcon icon={faGithub} /></div>
+                        <div className="left-nav-icon-title">Github</div>
+                    </a>
+                    <a className="left-nav-link" href="https://www.linkedin.com/in/isaac-nam-823180133/" target="_blank">
+                        <div className="left-nav-icon"><FontAwesomeIcon icon={faLinkedin} /></div>
+                        <div className="left-nav-icon-title">LinkedIn</div>
+                    </a>
+                    <a className="left-nav-link" href="" target="_blank">
+                        <div className="left-nav-icon"><FontAwesomeIcon icon={faAngellist} /></div>
+                        <div className="left-nav-icon-title">AngelList</div>
+                    </a>
+                    <a className="left-nav-link" href="" target="_blank">
+                        <div className="left-nav-icon"><FontAwesomeIcon icon={faFolderOpen} /></div>
+                        <div className="left-nav-icon-title">Portfolio</div>
+                    </a>
+                </div>
             </>
         );
     }
